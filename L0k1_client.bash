d6212fc36363dc4ce41a18a699c59fc57bc76108
Devin Weaver <suki@tritarget.org>
2012-08-10 15:15:04 -0400

    Adds logging

--- L0k1_client.bash
+++ L0k1_client.bash
@@ -4,8 +4,16 @@ source lib/random.bash
 # Settings
 source settings.bash
 
-# tell the server we are no longer running
-trap "{ curl -s \"${url}?poll=0\" > /dev/null ; exit 255; }" EXIT
+touch ${logFile}
+
+endIt() {
+  # tell the server we are no longer running
+  curl -s "${url}?poll=0" > /dev/null
+  echo "$(date): Shutting down" > ${logFile}
+  exit 0
+}
+
+trap endIt EXIT
 
 while true; do
   # Get bias percentile
@@ -24,14 +32,19 @@ while true; do
   pollingIterval=$((randomBetweenAnswer*60))
 
   # Send pollingIterval to server and examine response if tunnel should start
-  if $(curl -s "${url}?poll=${pollingIterval}") | grep "tunnel:true" >/dev/null 2>&1; then
+  if curl -s "${url}?poll=${pollingIterval}" | grep "tunnel:true" >/dev/null 2>&1; then
+    echo "$(date): Tunnel requested" > ${logFile}
     # Check if tunnel already running
     if $(ps ax ${pid}) | grep -q -v "ssh"; then
       # start tunnel
       ssh -f -N ${sshTunnelArgs} -p ${sshTunnelPort} ${sshTunnelHost} "sleep ${tunnelWaitTime}" >/dev/null 2>&1 &
       pid=$!
+      echo "$(date): Tunnel started (${pid})" > ${logFile}
+    else
+      echo "$(date): Tunnel already running (${pid}), ignoring" > ${logFile}
     fi
   fi
 
+  echo "$(date): Polling in ${pollingIterval} seconds" > ${logFile}
   sleep ${pollingIterval}
 done
